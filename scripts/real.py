76dcc0b1061ff9d591455949f1e6a95cbaef7cee
chazz7 <csimsii@mit.edu>
2014-02-11 05:21:54 -0500

    debug

--- scripts/real.py
+++ scripts/real.py
@@ -36,18 +36,16 @@ def doFormat(filename, key, skipRow=True, save=False, find_max=False):
             
             point['values'] = values
             points.append(point)
-            print db
             if find_max:
                 max_val = max(values)
                 if current_max == None or max_val > current_max:
                     current_max = max_val
             if save:
                 point['key'] = key
-                db.points.insert(point)
+                # db.points.insert(point)
     
     if find_max:
         db_max = { "key" : key, "value" : current_max }
-        print db
         maxes = db['maxes']
         maxes.insert(db_max)
         print db_max
@@ -60,15 +58,15 @@ path = "../data/"
 
 def saveCalls():
     # doFormat(path + "SumCallsDaily15days.csv", "sumCalls", save=True)
-    doFormat(path + "SumCallsDaily15days.csv", "sumCalls", find_max=True)
+    doFormat(path + "SumCallsDaily15days.csv", "sumCalls", save=True, find_max=True)
 
 def saveHome():
     # doFormat(path + "DivHomeDaily15days.csv", "divHome", save=True)
-    doFormat(path + "DivHomeDaily15days.csv", "divHome", find_max=True)
+    doFormat(path + "DivHomeDaily15days.csv", "divHome", save=True, find_max=True)
 
 def savePeople():
     # doFormat(path + "DivPeopleDaily.csv", "divPeople", save=True)
-    doFormat(path + "DivPeopleDaily.csv", "divPeople", find_max=True)
+    doFormat(path + "DivPeopleDaily.csv", "divPeople", save=True, find_max=True)
 
 saveCalls()
 saveHome()
