be6a0bd2cd9ab57cf42043a3874114efcd3f42d6
chazz7 <csimsii@mit.edu>
2014-02-11 05:15:43 -0500

    debug

--- scripts/real.py
+++ scripts/real.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient
 import csv
 
-def doFormat(filename, key, skipRow=True, save=False):
+def doFormat(filename, key, skipRow=True, save=False, find_max=False):
+    current_max = None
     db = None
     if save:
         client = MongoClient()
@@ -35,29 +36,39 @@ def doFormat(filename, key, skipRow=True, save=False):
             
             point['values'] = values
             points.append(point)
+            if find_max:
+                max_val = max(values)
+                if current_max == None or max_val > current_max:
+                    current_max = max_val
             if save:
                 point['key'] = key
                 db.points.insert(point)
-        
-    print points
-    return points
+    
+    if find_max:
+        db_max = { "key" : key, "value" : current_max }
+        # db.maxes.insert(db_max)
+        print db_max
+
     # print points
-    # return points
+    return points
+
 
 path = "../data/"
 
 def saveCalls():
-    doFormat(path + "SumCallsDaily15days.csv", "sumCalls", save=True)
+    # doFormat(path + "SumCallsDaily15days.csv", "sumCalls", save=True)
+    doFormat(path + "SumCallsDaily15days.csv", "sumCalls", find_max=True)
 
 def saveHome():
-    doFormat(path + "DivHomeDaily15days.csv", "divHome", save=True)
+    # doFormat(path + "DivHomeDaily15days.csv", "divHome", save=True)
+    doFormat(path + "DivHomeDaily15days.csv", "divHome", find_max=True)
 
 def savePeople():
-    doFormat(path + "DivPeopleDaily.csv", "divPeople", save=True)
+    # doFormat(path + "DivPeopleDaily.csv", "divPeople", save=True)
+    doFormat(path + "DivPeopleDaily.csv", "divPeople", find_max=True)
 
-
-#doFormat(path + "SumCallsDaily15days.csv")
-#doFormat(path + "DivHomeDaily15days.csv")
-#doFormat(path + "DivPeopleDaily.csv")
+saveCalls()
+saveHome()
+savePeople()
 
 
